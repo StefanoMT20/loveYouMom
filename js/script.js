d77d96228f99838e72ae6d04dd165395710ae24e
Stefano Meza <pieromezatejada14@hotmail.com>
2021-05-08 02:33:11 -0500

    main page finished

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ function onClickMenu(){
 	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+var swiper = new Swiper(".mySwiper", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	loop: true,
+	autoplay: {
+	  delay: 2500,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });

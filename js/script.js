714a4ed695348c2318778786f3a07adb9f57cc9e
Stefano Meza <pieromezatejada14@hotmail.com>
2021-05-08 23:24:18 -0500

    galeria

--- js/script.js
+++ js/script.js
@@ -21,3 +21,20 @@ var swiper = new Swiper(".mySwiper", {
 	  prevEl: ".swiper-button-prev",
 	},
   });
+
+  var swiper2 = new Swiper(".mySwiper-2", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+	  rotate: 50,
+	  stretch: 0,
+	  depth: 100,
+	  modifier: 1,
+	  slideShadows: true,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+  });
